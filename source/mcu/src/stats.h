cab90c3340a7434fe00dc7d0653679bac76eb353
Dmitriy Lipovyy <lipovyy@inp.nsk.su>
2022-02-09 18:23:08 +0700

    Transfer changes from 'ferrite/waveforms_develop'

--- source/mcu/src/stats.h
+++ source/mcu/src/stats.h
@@ -14,12 +14,19 @@ typedef struct {
 } AdcStats;
 
 typedef struct {
+    uint32_t buff_was_empty;
+    uint32_t buff_was_full;
+} DacWfStats;
+
+typedef struct {
 #ifdef GENERATE_SYNC
     uint32_t clock_count;
 #endif
     uint32_t sample_count;
     uint32_t max_intrs_per_sample;
     AdcStats adcs[SKIFIO_ADC_CHANNEL_COUNT];
+    DacWfStats dac_wf;
+    uint32_t adc_buff_was_full[SKIFIO_ADC_CHANNEL_COUNT];
 } Statistics;
 
 
--- tornado/ioc/fakedev/test.py
+++ tornado/ioc/fakedev/test.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import List
 
 import time
+from math import ceil
 from pathlib import Path
 
 from ferrite.utils.epics.ioc import make_ioc
@@ -19,6 +20,13 @@ class Handler(FakeDev.Handler):
 
     def __init__(self) -> None:
         self.channels = [0.0, 1.0, -1.0, 3.1415, -10.0, 10.0]
+
+        self.adc_count = 6
+        self.adc_wf_size = 0
+        self.adc_wfs = [[] for i in range(self.adc_count)]
+
+        self.dac_wf_size = 0
+        self.dac_wfs = []
 
     def write_dac(self, voltage: float) -> None:
         self.channels[0] = voltage
@@ -43,14 +51,75 @@ def run(epics_base_dir: Path, ioc_dir: Path, arch: str) -> None:
         time.sleep(scan_period)
         assert_synchronized(prefix, handler)
 
+        handler.dac_wf_size = int(ca.get(prefix, "aao0.NELM"))
+        print("AAO SIZE = ", handler.dac_wf_size)
+        dac_wf = []
+        dac_wf.append([i for i in range(handler.dac_wf_size)])
+        dac_wf.append([i for i in range(handler.dac_wf_size, 0, -1)])
+        dac_wf.append([5 for i in range(int(handler.dac_wf_size / 2))])
+
         some_val = 2.718
         ca.put(prefix, "ao0", some_val)
+
         time.sleep(scan_period)
+
         assert_eq(handler.channels[0], some_val)
+
+        #============
+        dac_waveform_sleep = 1.5
+
+        ca.put(prefix, "aao0", dac_wf[0], array=True)
+
+        time.sleep(dac_waveform_sleep)
+
+        assert len(handler.dac_wfs) == 1
+        assert handler.dac_wfs[len(handler.dac_wfs) - 1] == dac_wf[0]
+
+        ca.put(prefix, "aao0", dac_wf[1], array=True)
+        ca.put(prefix, "aao0", dac_wf[2], array=True)
+
+        time.sleep(dac_waveform_sleep * 2)
+
+        assert len(handler.dac_wfs) == 3
+        assert handler.dac_wfs[len(handler.dac_wfs) - 2] == dac_wf[1]
+        assert handler.dac_wfs[len(handler.dac_wfs) - 1] == dac_wf[2]
+
+        time.sleep(dac_waveform_sleep)
+
+        assert len(handler.dac_wfs) == 3
+
+        #=============
 
         some_val = 1.618
         handler.channels[0] = some_val
         time.sleep(scan_period)
         assert_eq(ca.get(prefix, f"ai0"), some_val)
+
+        #=============
+        handler.adc_wf_size = int(ca.get(prefix, "aai0.NELM"))
+        adc_waveform_sleep = FakeDev.poll_ms_timeout / 1000 * (handler.adc_wf_size / FakeDev.adc_wf_msg_max_elems)
+        adc_waveform_sleep = float(int(ceil(adc_waveform_sleep)))
+
+        for i in range(handler.adc_count):
+            handler.adc_wfs[i] = [x for x in range(handler.adc_wf_size * 2)]
+
+        adc_wf_numb = 0
+
+        time.sleep(adc_waveform_sleep)
+
+        adc_wf = [[] for i in range(handler.adc_count)]
+        for i in range(handler.adc_count):
+            print("aai%d:" % i)
+            adc_wf[i] = ca.get(prefix, "aai%d" % i, array=True)
+            assert handler.adc_wfs[i][(adc_wf_numb * handler.adc_wf_size):((adc_wf_numb + 1) * handler.adc_wf_size)] == adc_wf[i]
+
+        adc_wf_numb += 1
+
+        time.sleep(adc_waveform_sleep)
+
+        for i in range(handler.adc_count):
+            print("aai%d:" % i)
+            adc_wf[i] = ca.get(prefix, "aai%d" % i, array=True)
+            assert handler.adc_wfs[i][(adc_wf_numb * handler.adc_wf_size):((adc_wf_numb + 1) * handler.adc_wf_size)] == adc_wf[i]
 
     print("Test passed!")
