c35fe5609fb17540790e9986d81cb8c8fcc0dbc8
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-02-06 11:50:01 +0700

    Remove Ferrite deps for IOC

---
+++ source/ioc/iocBoot/iocTornado/st.cmd
@@ -0,0 +1,21 @@
+#!../../bin/linux-x86_64/Tornado
+
+#- You may have to change Tornado to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase("dbd/Tornado.dbd",0,0)
+Tornado_registerRecordDeviceDriver(pdbbase) 
+
+## Load record instances
+dbLoadRecords("db/devTornado.db")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit()
+
+## Start any sequence programs
+#seq sncTornado
