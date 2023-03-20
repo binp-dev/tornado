289a12269a1706dbc914a1296fed5d95b92ccaa7
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-03-20 19:34:13 +0700

    Remove Ferrite files

--- ferrite/ferrite/utils/strings.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-from typing import Any, Mapping
-
-
-def quote(text: str, char: str = '"') -> str:
-    return char + text.replace("\\", "\\\\").replace(char, "\\" + char) + char
