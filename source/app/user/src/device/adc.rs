53f3078f2eb68f118ec6a101ec5e8b4fe8b3fe4b
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-03-31 10:40:01 +0700

    Support scalar ADC

--- source/app/user/src/device/adc.rs
+++ source/app/user/src/device/adc.rs
@@ -1,33 +1,67 @@
 use super::Error;
 use crate::epics;
 use async_ringbuf::{AsyncHeapConsumer, AsyncHeapProducer, AsyncHeapRb};
-use common::values::{AdcPoint, Analog};
+use async_std::task::spawn;
+use common::values::{AdcPoint, Analog, Value};
 use ferrite::TypedVariable as Variable;
-use std::iter::ExactSizeIterator;
+use futures::future::try_join_all;
+use std::{
+    iter::ExactSizeIterator,
+    sync::{atomic::Ordering, Arc},
+};
 
 pub struct Adc {
+    array: AdcArray,
+    scalar: AdcScalar,
+}
+
+struct AdcArray {
     input: AsyncHeapConsumer<AdcPoint>,
-    output_array: Variable<[f64]>,
+    output: Variable<[f64]>,
+}
+
+struct AdcScalar {
+    input: Arc<<AdcPoint as Value>::Atomic>,
+    output: Variable<f64>,
 }
 
 pub struct AdcHandle {
-    pub buffer: AsyncHeapProducer<AdcPoint>,
+    buffer: AsyncHeapProducer<AdcPoint>,
+    last_point: Arc<<AdcPoint as Value>::Atomic>,
 }
 
 impl Adc {
     pub fn new(epics: epics::Adc) -> (Self, AdcHandle) {
         let buffer = AsyncHeapRb::<AdcPoint>::new(2 * epics.array.max_len());
         let (producer, consumer) = buffer.split();
+        let last = Arc::new(<AdcPoint as Value>::Atomic::default());
         (
             Self {
-                input: consumer,
-                output_array: epics.array,
+                array: AdcArray {
+                    input: consumer,
+                    output: epics.array,
+                },
+                scalar: AdcScalar {
+                    input: last.clone(),
+                    output: epics.scalar,
+                },
             },
-            AdcHandle { buffer: producer },
+            AdcHandle {
+                buffer: producer,
+                last_point: last,
+            },
         )
     }
-    pub async fn run(mut self) -> Result<(), Error> {
-        let max_len = self.output_array.max_len();
+    pub async fn run(self) -> Result<(), Error> {
+        try_join_all([spawn(self.array.run()), spawn(self.scalar.run())])
+            .await
+            .map(|_| ())
+    }
+}
+
+impl AdcArray {
+    async fn run(mut self) -> Result<(), Error> {
+        let max_len = self.output.max_len();
         loop {
             self.input.wait(max_len).await;
             if self.input.is_closed() {
@@ -35,7 +69,7 @@ impl Adc {
             }
             assert!(self.input.len() >= max_len);
             let input = self.input.as_mut_base();
-            self.output_array
+            self.output
                 .request()
                 .await
                 .write_from(input.pop_iter().map(AdcPoint::into_analog))
@@ -44,8 +78,36 @@ impl Adc {
     }
 }
 
+impl AdcScalar {
+    async fn run(mut self) -> Result<(), Error> {
+        loop {
+            self.output
+                .wait()
+                .await
+                .write(
+                    AdcPoint::try_from_base(self.input.load(Ordering::Acquire))
+                        .unwrap()
+                        .into_analog(),
+                )
+                .await;
+        }
+    }
+}
+
 impl AdcHandle {
     pub async fn push_iter<I: ExactSizeIterator<Item = AdcPoint>>(&mut self, points: I) {
-        self.buffer.push_iter(points).await.ok().unwrap()
+        let mut last = AdcPoint::default();
+        let len = points.len();
+        self.buffer
+            .push_iter(points.map(|x| {
+                last = x;
+                x
+            }))
+            .await
+            .ok()
+            .unwrap();
+        if len > 0 {
+            self.last_point.store(last.into_base(), Ordering::Release);
+        }
     }
 }
